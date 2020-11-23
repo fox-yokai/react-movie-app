81814730721e5839a53107684732f71be2472b77
fox-yokai <pat.w.cain@gmail.com>
2020-11-23 16:07:13 -0600

    created AddFavorites component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
+import AddFavorites from './components/AddFavorites';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -27,11 +28,11 @@ const App = () => {
 	return (
 		<div className='container-fluid movie-app'>
 			<div className='row d-flex align-items-center mt-4 mb-4'>
-        <MovieListHeading heading='Movies' />
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-        </div>
-        <div className='row'>
-        <MovieList movies={movies} />
+          <MovieListHeading heading='Movies' />
+          <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+          <div className='row'>
+        <MovieList movies={movies} favoriteComponent={AddFavorites}/>
 			</div>
 		</div>
 	);
